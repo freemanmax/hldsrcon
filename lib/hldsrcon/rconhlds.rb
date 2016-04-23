c01f4b3dcd8f28f00481759c486eb227ec798633
freemanmax <nenadstojkovikj@gmail.com>
2016-04-23 20:05:46 +0200

    Edit method rcon_pass

--- lib/hldsrcon/rconhlds.rb
+++ lib/hldsrcon/rconhlds.rb
@@ -13,14 +13,15 @@ class RconHlds
      return true
   end
   def rcon_pass(pass)
+     @pass = pass
      @socket.send "\xFF\xFF\xFF\xFFchallenge rcon", 0
      paket = @socket.recvfrom(1400)
      data = paket[0]
      data.gsub!("\xFF\xFF\xFF\xFFchallenge ", "")
      data.gsub!("\n\x00", "")
-     challenge = data
-     if ( challenge =~ /rcon\s\d\d\d\d\d\d\d\d\d/ )
-       @socket.send "\xFF\xFF\xFF\xFF#{challenge} #{pass}"
+     @challenge = data
+     if ( @challenge =~ /rcon\s\d\d\d\d\d\d\d\d\d/ )
+       @socket.send "\xFF\xFF\xFF\xFF#{@challenge} #{@pass}"
        rcon_paket = @socket.recvfrom(1400)
        if rcon_paket[0] == "\xFF\xFF\xFF\xFFl\x00\x00"
          @rcon_respons = "Good Rcon" 
